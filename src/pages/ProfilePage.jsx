24b9d41fe738529f44162faf42d776a0482e0148
KishorPokharel <kishorpokharel7@gmail.com>
2023-09-22 21:44:38 +0545

    logout

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const ProfilePage = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
   return (
     <div>
       <h2>Profile</h2>
       Your user account is activated.
+      <button onClick={() => handleLogout()}>Logout</button>
     </div>
   );
 };
