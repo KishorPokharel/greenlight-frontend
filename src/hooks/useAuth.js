f9579cb33d31af6a0f1b1a308d2202302e830ca5
KishorPokharel <kishorpokharel7@gmail.com>
2023-09-22 21:42:46 +0545

    auth hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,13 @@
 import ApiClient from '../service/api-client';
+import useSessionStore from '../store';
+import useAuthToken from './useAuthToken';
 
 const useAuth = () => {
+  const { removeToken, setToken, getToken } = useAuthToken();
+
+  const setSessionToken = useSessionStore((s) => s.setToken);
+  const removeSessionToken = useSessionStore((s) => s.setToken);
+
   const signup = (data) => {
     const apiClient = new ApiClient('/users');
     return apiClient.register(data);
@@ -11,7 +18,28 @@ const useAuth = () => {
     return apiClient.login(data);
   };
 
-  return { signup, login };
+  // removes from local storage and zustand store
+  const logout = () => {
+    removeToken(); // from localstorage
+    removeSessionToken(); // from memory
+  };
+
+  // saves to local storage and zustand store
+  const saveSession = (token) => {
+    setToken(token); // from localstorage
+    setSessionToken(token); // from memory
+  };
+
+  const loadSession = () => {
+    const token = getToken(); // from localstorage
+    setSessionToken(token); // from memory
+  };
+
+  const isLoggedIn = () => {
+    return !!useSessionStore((s) => s.token);
+  };
+
+  return { signup, login, logout, saveSession, loadSession, isLoggedIn };
 };
 
 export default useAuth;
